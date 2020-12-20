a5ee0180f1fe3aa08f5de5204e9d9ae3870b8f0d
efsam <E.salimi75@ymail.com>
2020-12-20 16:02:55 +0330

    commit Address in secondary

--- src/main/java/entities/Address.java
+++ src/main/java/entities/Address.java
@@ -16,4 +16,61 @@ public class Address {
     @Column(name = "state", nullable = false, length = 20)
     private String state;
 
+    @Column(name= "city", nullable = false, length = 20)
+    private String city;
+
+    @Column(name = "postal_address", nullable = false, length = 50)
+    private String postalAddress;
+
+    @Column(name = "postal_code", nullable = false, length = 10)
+    private String postalCode;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalAddress() {
+        return postalAddress;
+    }
+
+    public void setPostalAddress(String postalAddress) {
+        this.postalAddress = postalAddress;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
