21499d12f7e0aecf443706c0a618276ce17d40a4
efsam <E.salimi75@ymail.com>
2020-12-20 15:39:08 +0330

    commit Student in secondary

---
+++ src/main/java/entities/Student.java
@@ -0,0 +1,80 @@
+package entities;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false, length = 40)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 40)
+    private String lastName;
+
+    @Column(name = "student_code", nullable = false, unique = true, length = 50)
+    private String studentCode;
+
+    @Column(name = "birthday", nullable = false)
+    private Date birthday;
+
+    @ManyToMany
+    @JoinTable(name = "stu_tea",
+              joinColumns = @JoinColumn(name = "STUDENT_ID"),
+              inverseJoinColumns = @JoinColumn(name = "TEACHER_ID"))
+    private Set<Teacher> teachers;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Set<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(Set<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+}
