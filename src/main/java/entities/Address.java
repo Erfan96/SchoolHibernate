e0394316386ed3c6d002025ac45e349f89e8cc4f
efsam <E.salimi75@ymail.com>
2020-12-20 15:59:45 +0330

    commit Address in secondary

--- src/main/java/entities/Address.java
+++ src/main/java/entities/Address.java
@@ -1,4 +1,19 @@
 package entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "number", nullable = false, length = 7)
+    private Integer number;
+
+    @Column(name = "state", nullable = false, length = 20)
+    private String state;
+
 }
