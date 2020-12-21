576d1415c552b85fd62d453c1efaf9fbd514a638
efsam <E.salimi75@ymail.com>
2020-12-21 12:26:29 +0330

    commit App in secondary

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
+import dao.AddressDao;
 import dao.StudentDao;
 import dao.TeacherDao;
+import entities.Address;
 import entities.Student;
 import entities.Teacher;
 import util.JpaUtil;
@@ -8,56 +10,75 @@ import javax.persistence.EntityManager;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.HashSet;
 import java.util.Set;
-
-
 public class App {
+
+    private static StudentDao studentDao;
+    private static TeacherDao teacherDao;
+    private static AddressDao addressDao;
 
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
     public static void main(String[] args) {
         EntityManager entityManager = JpaUtil.getEntityManagerFactory().createEntityManager();
-        StudentDao studentDao = new StudentDao(entityManager);
-        TeacherDao teacherDao = new TeacherDao(entityManager);
+        initializeDao(entityManager);
 
         entityManager.getTransaction().begin();
 
-        Teacher teacher1 = new Teacher();
-        teacher1.setFirstName("Karim");
-        teacher1.setLastName("Mohammadi");
-        teacher1.setTeacherCode("T1");
-        teacher1.setSalary(2500.0);
-        teacher1.setBirthday(parseDate("1980-10-22"));
-        teacherDao.save(teacher1);
 
-        Teacher teacher2 = new Teacher();
-        teacher2.setFirstName("Karim2");
-        teacher2.setLastName("Mohammadi2");
-        teacher2.setTeacherCode("T2");
-        teacher2.setSalary(3000.0);
-        teacher2.setBirthday(parseDate("1980-10-22"));
-
-        teacherDao.save(teacher2);
-
-
-        Student student = new Student();
-        student.setFirstName("Ali");
-        student.setLastName("Karimi");
-        student.setStudentCode("S1");
-        student.setBirthday(parseDate("1996-05-06"));
-        Set<Teacher> teachers = new HashSet<Teacher>();
-        teachers.add(teacher1);
-        teachers.add(teacher2);
-        student.setTeachers(teachers);
-
-        studentDao.save(student);
+        createTeacher("Kazem", "Asadi", "t2", 30000.0, "1995-06-22",
+         createAddress(7778899, "tehran",
+                 "tehran", "kh lesani- k jam", "356-789-55"));
 
 
         entityManager.getTransaction().commit();
 
         entityManager.close();
         JpaUtil.shutdown();
+    }
+
+    private static void initializeDao(EntityManager entityManager) {
+        studentDao = new StudentDao(entityManager);
+        teacherDao = new TeacherDao(entityManager);
+        addressDao = new AddressDao(entityManager);
+    }
+
+    private static Student createStudent(String fName, String lName,
+             String code, String date, Set<Teacher> teachers, Set<Address> addresses){
+        Student student = new Student();
+        student.setFirstName(fName);
+        student.setLastName(lName);
+        student.setStudentCode(code);
+        student.setBirthday(parseDate(date));
+        student.setTeachers(teachers);
+        student.setAddresses(addresses);
+        studentDao.save(student);
+        return student;
+    }
+
+    private static Teacher createTeacher(String fName, String lName,
+         String code, Double salary, String date, Address address){
+        Teacher teacher = new Teacher();
+        teacher.setFirstName(fName);
+        teacher.setLastName(lName);
+        teacher.setTeacherCode(code);
+        teacher.setSalary(salary);
+        teacher.setBirthday(parseDate(date));
+        teacher.setAddress(address);
+        teacherDao.save(teacher);
+        return teacher;
+    }
+
+    private static Address createAddress(Integer number, String state,
+               String city, String postalAddress, String postalCode){
+        Address address = new Address();
+        address.setNumber(number);
+        address.setState(state);
+        address.setCity(city);
+        address.setPostalAddress(postalAddress);
+        address.setPostalCode(postalCode);
+        addressDao.save(address);
+        return address;
     }
 
     private static Date parseDate(String date){
