6bb202de7e0f2bcdcf31bd13e8f503b4c1d4a8fe
efsam <E.salimi75@ymail.com>
2020-12-21 12:25:04 +0330

    commit AddressDao in secondary

--- src/main/java/dao/AddressDao.java
+++ src/main/java/dao/AddressDao.java
@@ -3,9 +3,13 @@ package dao;
 import entities.Address;
 import javax.persistence.EntityManager;
 
-public class AddressDao extends EntityDao<Address>{
+public class AddressDao extends EntityDao<Address, Integer>{
 
     public AddressDao(EntityManager entityManager) {
         super(entityManager);
+    }
+
+    public Class<Address> getEntityClass() {
+        return Address.class;
     }
 }
