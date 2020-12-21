13c74646caad5e348f719f1a247a29fa5921d656
efsam <E.salimi75@ymail.com>
2020-12-21 12:24:14 +0330

    commit TeacherDao in secondary

--- src/main/java/dao/TeacherDao.java
+++ src/main/java/dao/TeacherDao.java
@@ -4,9 +4,13 @@ import entities.Teacher;
 
 import javax.persistence.EntityManager;
 
-public class TeacherDao extends EntityDao<Teacher>{
+public class TeacherDao extends EntityDao<Teacher, Integer>{
 
     public TeacherDao(EntityManager entityManager) {
         super(entityManager);
+    }
+
+    public Class<Teacher> getEntityClass() {
+        return Teacher.class;
     }
 }
